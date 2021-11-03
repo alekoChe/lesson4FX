29b2ffbb80bf9eee9a0f2ded146a1ee5707ff1d2
alekoChe <aleko2117bis@gmail.com>
2021-11-03 18:32:49 +0300

    data 03/11/2121

--- src/main/java/com/example/lesson4fx/HelloController.java
+++ src/main/java/com/example/lesson4fx/HelloController.java
@@ -4,8 +4,14 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class HelloController {
+    @FXML // если не повесить эту аннотацию над private, то поле не будет видно в других классах
+    private TextArea messageArea;
+    @FXML
+    private TextField messageField;
     @FXML
     private Label welcomeText;
 
@@ -15,5 +21,23 @@ public class HelloController {
         final Button source = (Button) actionEvent.getSource();
         welcomeText.setText("Welcome to JavaFX Application!");
         System.out.println("нажата кнопка " + source.getText());
+    }
+
+    public void clickSendButton(ActionEvent actionEvent) {
+        final String message = messageField.getText();
+        if (message.isEmpty()) {
+            return;
+        }
+        messageArea.appendText(message + "\n");
+        messageField.setText("");
+        messageField.requestFocus(); // возвращает фокус на поле
+    }
+
+    public void deleteText(ActionEvent actionEvent) {
+        messageArea.setText("");
+    }
+
+    public void exit(ActionEvent actionEvent) {
+        System.exit(0);
     }
 }
